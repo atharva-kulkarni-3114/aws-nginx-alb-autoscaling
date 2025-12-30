fca8a2c455f89315a89a9da9c5b1698fa3ddb271
Atharva Kulkarni <kulkarniatharva62@gmail.com>
2025-12-30 10:47:05 +0530

    Update User-Data-Script.sh

--- User-Data-Script.sh
+++ User-Data-Script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get install nginx -y
-sudo systemctl start nginx
-sudo systemctl enable nginx
-echo "<h1>Nginx running on EC2 via Auto Scaling</h1>" | sudo tee /var/www/html/index.html
+echo "Nginx Working Using AutoScaling">/var/www/html/index.html
+sudo systemctl restart nginx
